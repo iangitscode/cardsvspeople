2d73081b506775e0dc753aa6edd0415be53b5ed5
iangitscode <Jarvis@Friday.localdomain>
2018-10-21 18:23:15 -0400

    Kinda sorta have a server!

--- server/index.js
+++ server/index.js
@@ -1,2 +1,13 @@
-const koa = require('koa');
-const io = require('socket.io');
+const Koa = require('koa');
+const app = new Koa();
+const server = require('http').createServer(app.callback());
+const serve = require('koa-static');
+const io = require('socket.io')(server);
+
+app.use(serve('../client/public_html'));
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+server.listen(3000);
