f5ee9d2f37187e2e46eb4371577ef12dba248a88
Cameron Eldridge <cameldridge+git@gmail.com>
2018-10-21 18:10:27 -0400

    connect socket io

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { SocketProvider } from 'socket.io-react';
 
+const socket = io();
 const root = document.querySelector('#game');
 
 ReactDOM.render(
-  <>
+  <SocketProvider socket={socket}>
     Hello world
-  </>,
+  </SocketProvider>,
   root,
 );
