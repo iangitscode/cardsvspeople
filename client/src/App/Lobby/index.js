bf2ad9b21e1f42560940dbc11b36acc6f8392602
Cameron Eldridge <cameldridge+git@gmail.com>
2018-10-21 20:13:38 -0400

    Some socket handler stuff

--- client/src/App/Lobby/index.js
+++ client/src/App/Lobby/index.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import { socketConnect } from 'socket.io-react';
 import { connect } from 'react-redux';
-import { compose } from '../../helpers';
+import { compose, socketHandlers } from '../../helpers';
 
 export default compose(
   socketConnect,
+  socketHandlers({
+    receiveRoomName: roomName => { console.log(roomName); },
+  }),
   class Lobby extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
         roomName: '',
+        errorMsg: null,
       }
-      this.props.socket.on('receiveRoomName', (roomName) => {
-        console.log(roomName);
-      })
     }
 
     joinRoom() {
-      this.props.socket.emit('joinRoom', this.state.roomName);
+      this.props.socket.emit('joinRoom', this.state.roomName, ({ status, msg }) => {
+        if (status === 'error') {
+          this.setState({ errorMsg: msg });
+        }
+      });
     }
 
     createRoom() {
@@ -29,10 +34,12 @@ export default compose(
         <div>
           <input onChange={event => this.setState({ roomName: event.currentTarget.value })} />
           <button onClick={() => this.joinRoom()}>Join Room</button>
-          <br/>
+          <br />
           <button onClick={() => this.createRoom()}>Create Room</button>
+          <br />
+          { this.state.errorMsg }
         </div>
-      )
+      );
     }
   }
 )
--- client/src/helpers.js
+++ client/src/helpers.js
@@ -1,4 +1,35 @@
+import React from 'react';
+
 export function compose(...functions) {
-  let [item, ...funcs] = functions.reverse();
+  const [item, ...funcs] = functions.reverse();
   return funcs.reduce((o, f) => f(o), item);
+}
+
+export function socketHandlers(handlers) {
+  return Component => class extends React.Component {
+    constructor(props) {
+      super(props);
+      if (!this.props.socket) {
+        throw new Error('Socket prop is not defined');
+      }
+    }
+
+    componentDidMount() {
+      Object.entries(handlers)
+        .map(([name, handler]) => {
+          this.props.socket.on(name, handler);
+        });
+    }
+
+    componentWillUnmount() {
+      Object.entries(handlers)
+        .map(([name, handler]) => {
+          this.props.socket.removeListener(name, handler);
+        });
+    }
+
+    render() {
+      return <Component {...this.props} />
+    }
+  }
 }
