d42e5efdbd87786f746206b6c903e645a59b3a3e
Snipower <thereceiverofjunkmail@gmail.com>
2018-11-22 20:43:14 -0600

    Made it secure

--- client/src/app/socket-service/socket-service.ts
+++ client/src/app/socket-service/socket-service.ts
@@ -8,7 +8,7 @@ export class SocketService {
   private socket:SocketIOClient.Socket;
   constructor() { 
     // TODO: Change this to hosting ip address
-     this.socket = io('http://localhost:9001');
+     this.socket = io('https://localhost:9001');
     //this.socket = io('192.168.1.109:3000');
   }
   public getSocket():SocketIOClient.Socket {
